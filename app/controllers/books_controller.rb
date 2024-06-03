cc5a3b8a4925162851e088c6f4a453cc0db96c93
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-06-03 13:32:27 +0000

    [finish]sign up,log inページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,32 @@
 class BooksController < ApplicationController
   def new
+    @book=Book.new
+  end
+  
+  def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.user_name=current_user.name
+    @book.save
+    redirect_to book_path
   end
 
   def index
+    @book=Book.all
+    
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :books, only:[:new, :create, :index, :show, :destroy]
-  resources :users, only:[:edit, :show, :update]
+
   devise_for :users
   root to: "homes#top"
+  get 'homes/about', as: 'about'
+  resources :books, only:[:new, :create, :index, :show, :destroy]
+  resources :users, only:[:edit, :show, :update]
+ 
 end
