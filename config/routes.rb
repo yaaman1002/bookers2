523525de19c45cf40372a19658295e3b945d85e5
yaaman1002 <166231007+yaaman1002@users.noreply.github.com>
2024-06-12 12:15:43 +0000

    [update]RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: "homes#top"
-  get 'homes/about', as: 'about'
+  root to: "home#top"
+  get 'home/about', as: 'about'
   resources :books, only:[:edit, :create, :index, :show, :update, :destroy]
   resources :users, only:[:index, :edit, :show, :update]
   
